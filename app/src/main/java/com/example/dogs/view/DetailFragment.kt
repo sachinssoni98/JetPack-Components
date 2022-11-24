86727aeecb584e32af34778e8c2436c361ae8cdc
Sachin Soni <sachinssoni98@gmail.com>
2022-11-24 18:55:16 +0530

    Created Layout of ListFragment and Details Fragment

--- app/src/main/java/com/example/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogs/view/DetailFragment.kt
@@ -13,22 +13,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class DetailFragment : Fragment() {
-    lateinit var btn: FloatingActionButton
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_detail, container, false)
-        btn= view.findViewById(R.id.listBtn)
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        btn.setOnClickListener {
-            val action: NavDirections= DetailFragmentDirections.actionDetailFragmentToListFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
-    }
 }
--- app/src/main/java/com/example/dogs/view/ListFragment.kt
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -12,7 +12,6 @@ import com.example.dogs.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
-    lateinit var btn: FloatingActionButton
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,16 +19,6 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
 
         val view= inflater.inflate(R.layout.fragment_list, container, false)
-        btn= view.findViewById(R.id.detailsBtn)
         return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        btn.setOnClickListener {
-            val action:NavDirections= ListFragmentDirections.actionListFragmentToDetailFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
-
     }
 }
